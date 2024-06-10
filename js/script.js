8b54f3fa10cbef1d330ad0fe01975fcf82a01646
Dmytrii Datskevych <datskevychdv@gmail.com>
2024-06-10 23:10:06 +0200

    Fix images for adaptivity, add some functionality for buttons

---
+++ js/script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownOverlay = document.querySelector('.dropdown-overlay');
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const btnHamburger = document.querySelector('.hamburger');
+    const btnCloseDropdown = document.querySelector('.dropdown-btn-close');
+    const btnOrdre = document.querySelector('.promo-button');
+    const btnCloseModal = document.querySelector('.modal-btn-close');
+
+    const navList = document.querySelectorAll('.dropdown-nav-list');
+    const header = document.querySelector('.header');
+
+
+
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', () => {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            header.classList.add('show');
+        } else {
+            header.classList.remove('show');
+        }
+
+        lastScrollTop = scrollTop;
+    })
+
+
+    const openDropdown = () => {
+        dropdownOverlay.classList.add('is-open');
+    };
+
+    const openModal = () => {
+        modalOverlay.classList.add('is-open');
+    };
+
+    const closeDropdown = () => {
+        dropdownOverlay.classList.remove('is-open');
+    };
+
+    const closeModal = () => {
+        modalOverlay.classList.remove('is-open');
+    };
+
+    btnHamburger.addEventListener('click', openDropdown);
+
+    btnCloseDropdown.addEventListener('click', closeDropdown);
+
+    btnOrdre.addEventListener('click', openModal);
+
+    btnCloseModal.addEventListener('click', closeModal);
+
+    modalOverlay.addEventListener('click', closeModal);
+
+
+
+    navList.forEach(link => {
+        link.addEventListener('click', closeDropdown);
+    });
+
+});
